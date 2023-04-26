f977524dea65de52bfe99c2fcc581c157e9d3967
lucaruboni <lucaruboni@gmail.com>
2023-04-26 11:30:17 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+
+export default{
+  components: {
+    PageHeader,
+    PageMain,
+    PageFooter
+  }
+}
+
+
+</script>
+
+<template>
+  
+  <PageHeader></PageHeader>
+
+  <PageMain></PageMain>
+
+  <PageFooter></PageFooter>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,14 @@
+<script>
+export default{
+    name: 'PageHeader'
+}
+
+</script>
+
+<template>
+ 
+</template>
+
+<style lang="scss" scoped>
+
+</style>
