132feef938dae7769c7404c42fd74a0b867425b8
lucaruboni <lucaruboni@gmail.com>
2023-04-26 15:33:51 +0200

    [add] navbar

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -58,7 +58,39 @@ export default{
 
         <div class="bottom_header">
             <div class="jumbotron">
+                <div class="container">
+                    <div class="row">
 
+                                                            <!-- NAVBAR -->
+
+
+                        <nav class="navbar navbar-expand-lg bg-body-tertiary justify-content-between">
+                        
+                            <a class="navbar-brand text-light" href="#"><span>NEX</span>GEN</a>
+                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                              <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div class="" id="navbarNavAltMarkup">
+                              <div class="navbar-nav">
+                                <a class="nav-link active text-light" aria-current="page" href="#">HOME</a>
+                                <a class="nav-link text-light" href="#">ABOUT</a>
+                                <a class="nav-link text-light" href="#">SERVICE</a>
+                                <a class="nav-link text-light">PROCESS</a>
+                                <a class="nav-link text-light">TESTIMONIALS</a>
+                                <a class="nav-link text-light"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+                                </svg></a>
+                                <button class="btn btn-primary">GET IN TOUCH</button>
+                              </div>
+                            </div>
+                        
+                        </nav>
+
+                                                                    <!-- /NAVBAR -->
+
+
+                    </div>
+                </div>
             </div>                            
         </div>     
 
@@ -84,7 +116,11 @@ export default{
     .jumbotron{
         background-color:$bg-dark;
         height: 100%;
+
+   
     }
+
+   
 }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 import './components/scss/app.scss'
 import App from './App.vue'
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
 
-createApp(App).mount('#app')
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faUserSecret)
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
