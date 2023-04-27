eb99ba6dfccb5a39d5c1f822fc642e19b9df063c
lucaruboni <lucaruboni@gmail.com>
2023-04-27 18:42:42 +0200

    [fix] minor style change

--- src/components/FifthSection.vue
+++ src/components/FifthSection.vue
@@ -28,8 +28,11 @@ export default{
 
                     <div class="text-area text-light p-3 d-flex flex-column gap-4">
                         
-                        <p class="my_text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. In dolor, officiis similique impedit nisi laborum numquam soluta? Pariatur, distinctio! Voluptas dicta consequuntur, excepturi provident architecto perferendis veniam animi aspernatur quae quos quaerat quasi id velit ducimus suscipit perspiciatis ab culpa.
+                        <p class="my_text d-flex flex-column">
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam neque dolor iste quos! Quo nam dolorem et repellendus nesciunt sint atque excepturi, labore blanditiis repudiandae.
+                            <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-quote align-self-end" viewBox="0 0 16 16">
+                              <path d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 9 7.558V11a1 1 0 0 0 1 1h2Zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 3 7.558V11a1 1 0 0 0 1 1h2Z"/>
+                            </svg>
                         </p>
                     </div>
                 </div>
@@ -41,8 +44,11 @@ export default{
 
                     <div class="text-area text-light p-3 d-flex flex-column gap-4">
                         
-                        <p class="my_text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. In dolor, officiis similique impedit nisi laborum numquam soluta? Pariatur, distinctio! Voluptas dicta consequuntur, excepturi provident architecto perferendis veniam animi aspernatur quae quos quaerat quasi id velit ducimus suscipit perspiciatis ab culpa.
+                        <p class="my_text d-flex flex-column">
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam neque dolor iste quos! Quo nam dolorem et repellendus nesciunt sint atque excepturi, labore blanditiis repudiandae.
+                            <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-quote align-self-end" viewBox="0 0 16 16">
+                              <path d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 9 7.558V11a1 1 0 0 0 1 1h2Zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 3 7.558V11a1 1 0 0 0 1 1h2Z"/>
+                            </svg>
                         </p>
                     </div>
                 </div>
@@ -54,8 +60,11 @@ export default{
 
                     <div class="text-area text-light p-3 d-flex flex-column gap-4">
                         
-                        <p class="my_text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. In dolor, officiis similique impedit nisi laborum numquam soluta? Pariatur, distinctio! Voluptas dicta consequuntur, excepturi provident architecto perferendis veniam animi aspernatur quae quos quaerat quasi id velit ducimus suscipit perspiciatis ab culpa.
+                        <p class="my_text d-flex flex-column">
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam neque dolor iste quos! Quo nam dolorem et repellendus nesciunt sint atque excepturi, labore blanditiis repudiandae.
+                            <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-quote align-self-end" viewBox="0 0 16 16">
+                              <path d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 9 7.558V11a1 1 0 0 0 1 1h2Zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 3 7.558V11a1 1 0 0 0 1 1h2Z"/>
+                            </svg>
                         </p>
                     </div>
                 </div>
