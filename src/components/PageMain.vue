840791645040ff8cfac799a5673acfae7ae52ffe
lucaruboni <lucaruboni@gmail.com>
2023-04-27 15:47:35 +0200

    [add] second section as component

--- src/components/PageMain.vue
+++ src/components/PageMain.vue
@@ -1,9 +1,11 @@
 <script>
 import FirstSection from './FirstSection.vue'
+import SecondSection from './SecondSection.vue'
 export default{
     name: 'PageMain',
     components: {
-        FirstSection
+        FirstSection,
+        SecondSection
     }
 }
 
@@ -25,90 +27,7 @@ export default{
 
                                                         
     <section class="bg_dark">
-
-        <div class="container">
-            <div class="row">
-               
-                    <div class="col-8 text-light ">
-                            <h6 class="text_primary">ROAD TRANSPORT</h6>
-                            <h1>Main Services</h1>
-                            <p id="presentation_p">With all of this expertise and capability comes an unrivalled commitment to customer service. We will work hard to understand your needs in order to develop a productive, long-term partnership.</p>
-                            
-                                
-                                
-                            
-                    </div>      
-                    <div class="col-4 d-flex">
-                        <button class="btn btn_border align-self-center text-light">SEE ALL</button>
-                    </div>
-            </div>
-            <div class="row row-cols-4 justify-content-between">
-                <div class="col bg_grey ">
-                    <div class="icon_row d-flex justify-content-between">
-                        <svg  xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-truck bg_primary_transparent" viewBox="0 0 16 16">
-                          <path d="M0 3.5A1.5 1.5 0 0 1 1.5 2h9A1.5 1.5 0 0 1 12 3.5V5h1.02a1.5 1.5 0 0 1 1.17.563l1.481 1.85a1.5 1.5 0 0 1 .329.938V10.5a1.5 1.5 0 0 1-1.5 1.5H14a2 2 0 1 1-4 0H5a2 2 0 1 1-3.998-.085A1.5 1.5 0 0 1 0 10.5v-7zm1.294 7.456A1.999 1.999 0 0 1 4.732 11h5.536a2.01 2.01 0 0 1 .732-.732V3.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .294.456zM12 10a2 2 0 0 1 1.732 1h.768a.5.5 0 0 0 .5-.5V8.35a.5.5 0 0 0-.11-.312l-1.48-1.85A.5.5 0 0 0 13.02 6H12v4zm-9 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm9 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
-                        </svg>
-
-                        <svg  xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-arrow-right-short text_primary" viewBox="0 0 16 16">
-                          <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
-                        </svg>
-
-                    </div>
-
-                    <div class="text-area text-light">
-                        <h3>
-                            Technology
-                        </h3>
-                        <p>
-                            We are continually focused on developing technology solutions adapted to our client's needs.
-                        </p>
-                    </div>
-                </div>
-                <div class="col bg_grey ">
-                    <div class="icon_row d-flex justify-content-between">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-thermometer-snow bg_primary_transparent" viewBox="0 0 16 16">
-                          <path d="M5 12.5a1.5 1.5 0 1 1-2-1.415V9.5a.5.5 0 0 1 1 0v1.585A1.5 1.5 0 0 1 5 12.5z"/>
-                          <path d="M1 2.5a2.5 2.5 0 0 1 5 0v7.55a3.5 3.5 0 1 1-5 0V2.5zM3.5 1A1.5 1.5 0 0 0 2 2.5v7.987l-.167.15a2.5 2.5 0 1 0 3.333 0L5 10.486V2.5A1.5 1.5 0 0 0 3.5 1zm5 1a.5.5 0 0 1 .5.5v1.293l.646-.647a.5.5 0 0 1 .708.708L9 5.207v1.927l1.669-.963.495-1.85a.5.5 0 1 1 .966.26l-.237.882 1.12-.646a.5.5 0 0 1 .5.866l-1.12.646.884.237a.5.5 0 1 1-.26.966l-1.848-.495L9.5 8l1.669.963 1.849-.495a.5.5 0 1 1 .258.966l-.883.237 1.12.646a.5.5 0 0 1-.5.866l-1.12-.646.237.883a.5.5 0 1 1-.966.258L10.67 9.83 9 8.866v1.927l1.354 1.353a.5.5 0 0 1-.708.708L9 12.207V13.5a.5.5 0 0 1-1 0v-11a.5.5 0 0 1 .5-.5z"/>
-                        </svg>
-
-                        <svg  xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-arrow-right-short text_primary" viewBox="0 0 16 16">
-                          <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
-                        </svg>
-
-                    </div>
-
-                    <div class="text-area text-light">
-                        <h3>
-                            Reefer Cargo
-                        </h3>
-                        <p>
-                            Regular and frequent monitoring from the receipt of the loaded container to final destination.
-                        </p>
-                    </div>
-                </div>
-                <div class="col bg_grey ">
-                    <div class="icon_row d-flex justify-content-between">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-boxes bg_primary_transparent" viewBox="0 0 16 16">
-                          <path d="M7.752.066a.5.5 0 0 1 .496 0l3.75 2.143a.5.5 0 0 1 .252.434v3.995l3.498 2A.5.5 0 0 1 16 9.07v4.286a.5.5 0 0 1-.252.434l-3.75 2.143a.5.5 0 0 1-.496 0l-3.502-2-3.502 2.001a.5.5 0 0 1-.496 0l-3.75-2.143A.5.5 0 0 1 0 13.357V9.071a.5.5 0 0 1 .252-.434L3.75 6.638V2.643a.5.5 0 0 1 .252-.434L7.752.066ZM4.25 7.504 1.508 9.071l2.742 1.567 2.742-1.567L4.25 7.504ZM7.5 9.933l-2.75 1.571v3.134l2.75-1.571V9.933Zm1 3.134 2.75 1.571v-3.134L8.5 9.933v3.134Zm.508-3.996 2.742 1.567 2.742-1.567-2.742-1.567-2.742 1.567Zm2.242-2.433V3.504L8.5 5.076V8.21l2.75-1.572ZM7.5 8.21V5.076L4.75 3.504v3.134L7.5 8.21ZM5.258 2.643 8 4.21l2.742-1.567L8 1.076 5.258 2.643ZM15 9.933l-2.75 1.571v3.134L15 13.067V9.933ZM3.75 14.638v-3.134L1 9.933v3.134l2.75 1.571Z"/>
-                        </svg>
-                        <svg  xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-arrow-right-short text_primary" viewBox="0 0 16 16">
-                          <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
-                        </svg>
-
-                    </div>
-
-                    <div class="text-area text-light">
-                        <h3>
-                            Dry Cargo
-                        </h3>
-                        <p>
-                            We work with most types of dry cargo to the most dangerous requiring care.
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-
+        <SecondSection></SecondSection>
     </section>
 
 
@@ -124,33 +43,8 @@ export default{
 <style lang="scss" scoped>
 @use './scss/partials/variables.scss' as *;
   
-    .bg_dark{
-        background-color: $bg-primary;
-    }
+  .bg_dark{
+      background-color: $bg-primary;
+  }
 
-    .bg_grey{
-        background-color: $bg-grey;
-    }
-
-    .text_primary{
-        color: $primary;
-    }
-
-    .bg_primary{
-        background-color: $primary;
-    }
-
-    .bg_primary_transparent{
-        background-color: $primary-transparent;
-        border-radius: 50%;
-        padding: 1rem;
-    }
-    .btn_border{
-        border-color: $primary;
-        border-radius: 3px;
-    }
-
-    .icon_row{
-        color: $primary;
-    }
 </style>
